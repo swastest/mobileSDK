2157b75fa65a327ff75d28d3b419e52909d2152b
Tatiana Kazakova <test.kazakova@gmail.com>
2022-06-19 20:49:57 +0300

    local start

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,38 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import driver.LocalMobileDriver;
+import helper.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static io.qameta.allure.Allure.step;
+
+public class TestBase {
+    @BeforeAll
+    public static void setup() {
+        Configuration.browser = LocalMobileDriver.class.getName();
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    public void startDriver() {
+        addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+
+        step("Close driver", Selenide::closeWebDriver);
+
+    }
+}
